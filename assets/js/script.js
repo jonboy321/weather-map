3eb06b702c4701ae8f59536534447d3d02e83f09
Jonny Gemme <gemmejonathan@gmail.com>
2022-04-10 21:08:03 -0400

    Setting Up All Files

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+const searchHistory = [];
+const weatherApiUrl = "https://api.openweathermap.org/";
+const apiKey = "b3c112276882c79713431e06ab1557f1";
+
+let searchForm = document.getElementById('search-form');
