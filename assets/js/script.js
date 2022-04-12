05da39394d9b5dda6926bcf3a6a325d8d3fe87fd
Jonny Gemme <gemmejonathan@gmail.com>
2022-04-11 21:47:57 -0400

    Trying to figure out how to make API work in a function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,10 @@
 const searchHistory = [];
-const weatherApiUrl = "https://api.openweathermap.org/";
+const weatherApiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=30&lon=35&appid=";
 const apiKey = "b3c112276882c79713431e06ab1557f1";
+const searchQuery = document.getElementById('site-search');
 
 let searchForm = document.getElementById('search-form');
+
+fetch(weatherApiUrl + apiKey)
+.then(response => response.json())
+.then(data => console.log(data));
